246e2874ac553d188dfba7fb13c20f8ec4fafda7
RH <f2hodge@gmail.com>
2023-07-16 19:26:35 -0400

    20230716 19:26

--- javascript_crash/main.js
+++ javascript_crash/main.js
@@ -18,7 +18,7 @@
 // const items = document.querySelectorAll('.item'); // select all in item class
 // items.forEach((item) => console.log(item)); // print the items(elements)
 
-const ul = document.querySelector('.items');
+// const ul = document.querySelector('.items');
 
 // ul.remove(); // removes the ul from the page
 // ul.lastElementChild.remove(); // removes just the last element
@@ -26,11 +26,41 @@ const ul = document.querySelector('.items');
 // ul.children[1].innerText = "innerText";
 // ul.lastElementChild.innerHTML = '<h1>H1 Text</h1>';
 
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 // btn.style.background = 'red'; // can change style properties too
-btn.addEventListener('click', (e) => {
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   // console.log(e);
+//   document.querySelector('#my-form').style.background = '#ccc';
+//   document.querySelector('body').classList.add('bg-dark');
+// });
+
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
+
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
   e.preventDefault();
-  // console.log(e);
-  document.querySelector('#my-form').style.background = '#ccc';
-  document.querySelector('body').classList.add('bg-dark');
-});
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "Please enter all fields";
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+
+    userList.appendChild(li);
+
+    // Clear fields
+    nameInput.value = "";
+    emailInput.value = "";
+  }
+}
