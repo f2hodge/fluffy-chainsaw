a920e7949aebafe258002d38bfce7d58ebe16534
Ryan Hodge <f2hodge@gmail.com>
2023-07-16 19:01:18 -0400

    20230716 19:01

--- javascript_crash/main.js
+++ javascript_crash/main.js
@@ -3,66 +3,34 @@
 // console.error("This is an error");
 // console.warn("This is a warning");
 
-// Object Oriented Programming
-//
+// Single-element selectors
+// console.log(document.getElementById('my-form'));
+// const form = document.getElementById('my-form'); // can assign element to variable
+// console.log(form); // this will output the same as above
+// querySelector is newer and can select anything
+// console.log(document.querySelector('h1')); // outputs only the first h1 element
 
-// Constructor functions with prototypes
-/*
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dob = new Date(dob); // can call the Date constructor in this one to produce a real date
-  // this.getBirthYear = () => {      --> Doing it this way puts methods in every instance
-  //   return this.dob.getFullYear();
-  // };
-  // this.getFullName = () => {       --> Same as above, see below for new way
-  //   return `${this.firstName} ${this.lastName}`;
-  // };
-};
+// Multi-element selectors
+// console.log(document.querySelectorAll('.item')); // generally want to use this not the following
+// document.getElementsByClassName('item'); // don't need the . because class is already specified
+// document.getElementsByTagName('li'); // selecting all by tag name
 
-Person.prototype.getBirthYear = function() { // --> Removed from above (this is preferred)
-  return this.dob.getFullYear();
-};
-Person.prototype.getFullName = function() { // --> Creating methods this way puts them in prototype of Objects
-  return `${this.firstName} ${this.lastName}`;
-};
+// const items = document.querySelectorAll('.item'); // select all in item class
+// items.forEach((item) => console.log(item)); // print the items(elements)
 
+const ul = document.querySelector('.items');
 
-// Instantiation
-const person1 = new Person("Ted", "Darby", "04/03/1980");
-const person2 = new Person("Bill", "Buffalo", "07/09/1975");
-const person3 = new Person("Sara", "Carmichael", "01/25/1992");
+// ul.remove(); // removes the ul from the page
+// ul.lastElementChild.remove(); // removes just the last element
+// ul.firstElementChild.textContent = "textContent";
+// ul.children[1].innerText = "innerText";
+// ul.lastElementChild.innerHTML = '<h1>H1 Text</h1>';
 
-// console.log(person1);
-// console.log(person2.dob.getFullYear());
-// console.log(person3.getBirthYear());
-console.log(person1.getFullName());
-*/
-//
-// End ES5 (previous version) of OOP in JavaScript
-// Begin OOP in ES6
-// Now Classes are added: does the same under the hood as prototype-version
-// Class
-class Persona {
-  constructor(firstName, lastName, dob) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.dob = new Date(dob);
-  }
-
-  // This is the syntactic sugar where this is placed within the class but appears in prototype
-  getBirthYear() {
-    return this.dob.getFullYear();
-  };
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  };
-};
-
-const person5 = new Persona("Dwight", "Eisenhower", "03/14/1963");
-const person6 = new Persona("Barbara", "Bush", "09/19/1945");
-const person7 = new Persona("Margaret", "Thatcher", "11/23/1938");
-
-console.log(person5.lastName.length);
-console.log(person6.getFullName());
-console.log(person7.getBirthYear());
+const btn = document.querySelector('.btn');
+// btn.style.background = 'red'; // can change style properties too
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  // console.log(e);
+  document.querySelector('#my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
+});
--- javascript_crash/object_demo.js
+++ javascript_crash/object_demo.js
@@ -51,3 +51,68 @@ console.log(todo[1].text); // selecting a value through the Array and then Objec
 // convert above data to JSON for more versatile use
 const todoJSON = JSON.stringify(todo);
 console.log(todoJSON);
+
+
+// Object Oriented Programming
+//
+
+// Constructor functions with prototypes
+/*
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob); // can call the Date constructor in this one to produce a real date
+  // this.getBirthYear = () => {      --> Doing it this way puts methods in every instance
+  //   return this.dob.getFullYear();
+  // };
+  // this.getFullName = () => {       --> Same as above, see below for new way
+  //   return `${this.firstName} ${this.lastName}`;
+  // };
+};
+
+Person.prototype.getBirthYear = function() { // --> Removed from above (this is preferred)
+  return this.dob.getFullYear();
+};
+Person.prototype.getFullName = function() { // --> Creating methods this way puts them in prototype of Objects
+  return `${this.firstName} ${this.lastName}`;
+};
+
+
+// Instantiation
+const person1 = new Person("Ted", "Darby", "04/03/1980");
+const person2 = new Person("Bill", "Buffalo", "07/09/1975");
+const person3 = new Person("Sara", "Carmichael", "01/25/1992");
+
+// console.log(person1);
+// console.log(person2.dob.getFullYear());
+// console.log(person3.getBirthYear());
+console.log(person1.getFullName());
+*/
+//
+// End ES5 (previous version) of OOP in JavaScript
+// Begin OOP in ES6
+// Now Classes are added: does the same under the hood as prototype-version
+// Class
+class Persona {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+  }
+
+  // This is the syntactic sugar where this is placed within the class but appears in prototype
+  getBirthYear() {
+    return this.dob.getFullYear();
+  };
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  };
+};
+
+const person5 = new Persona("Dwight", "Eisenhower", "03/14/1963");
+const person6 = new Persona("Barbara", "Bush", "09/19/1945");
+const person7 = new Persona("Margaret", "Thatcher", "11/23/1938");
+
+console.log(person5.lastName.length);
+console.log(person6.getFullName());
+console.log(person7.getBirthYear());
